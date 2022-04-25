11d4f79c5fe706b92143fe9dbab3739750914443
Hugo Fabris <hugomfabris@gmail.com>
2022-04-25 15:10:40 -0300

    Sixth commit - Added comments to Meal Time problem

--- Problem sets/Week 1/05 - Meal Time/meal.py
+++ Problem sets/Week 1/05 - Meal Time/meal.py
@@ -1,3 +1,4 @@
+#First we define a function to take user’s input formatted in 24-hour time as #:## or ##:##
 def main():
     userTime = input("What time is it? ")
     if (convert(userTime) >= 7 and convert(userTime) <= 8.0):
@@ -7,7 +8,7 @@ def main():
     elif (convert(userTime) >= 18 and convert(userTime) <= 19.0):
         print("dinner time")
 
-
+#Here we define the function that we'll be using inside of main to convert the user's input format into float, thereby helping us to buid the main function logic
 def convert(time):
     hours, minutes = time.split(":")
     hours = float(hours)
