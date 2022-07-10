23dae79213e986fe7f71059d073ba559a244d4bd
Hugo Fabris <hugomfabris@gmail.com>
2022-07-10 08:12:55 -0300

    Nineteenth commit - Modifications in Taqueria's, problem set week 3

--- Problem set/Week 3/02 - Felipe’s Taqueria/taqueria.py
+++ Problem set/Week 3/02 - Felipe’s Taqueria/taqueria.py
@@ -1,3 +1,4 @@
+#This dict represents the taqueria's catalogue
 menu = {
     "Baja Taco": 4.00,
     "Burrito": 7.50,
@@ -11,18 +12,22 @@ menu = {
 }
 
 total = 0
+#This while loop will prompt the user again until "crtl d" keys are pressed
 while True:
     try:
-        item = input("Item: ").strip().capitalize()
+        item = input("Item: ").strip().title()
         if item in menu:
             total += menu[item]
+            print("Total: $", end="")
+            print("{:.2f}".format(total))
+    #The except block get us out of the loop
     except EOFError:
-        print(f"Total: ${total}")
+        print()
+        print(f"Total: ${total:.2f}")
         break
     except KeyError:
         pass
-        
-  
 
-    
-    
+
+
+
